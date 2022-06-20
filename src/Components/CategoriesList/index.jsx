cdd5f03aebdad1cacd49f14e18198e5dd89614d3
omarmohsen179 <mohsenomar350@gmail.com>
2022-06-20 15:05:09 +0200

    abdallah

--- src/Components/CategoriesList/index.jsx
+++ src/Components/CategoriesList/index.jsx
@@ -1,96 +1,105 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
+import { getCategories } from "../../Views/Unkown/Home/Components/Api";
 import Image from "../Image";
 import TitleHeader from "../TitleHeader";
 import "./index.css";
 function CategoryList({ data = [], style, title }) {
-  const datax = [
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
-    },
-    {
-      image:
-        "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
-    },
-  ];
+  // const datax = [
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/GooglePlay-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/itunes-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Playstation-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/Minecraft-logo.jpg",
+  //   },
+  //   {
+  //     image:
+  //       "https://183152-537558-raikfcquaxqncofqfm.stackpathdns.com/pub/media/wysiwyg/prepaid-cards/logos/skype-logo.jpg",
+  //   },
+  // ];
+
+  const [datax, setData] = useState();
+  console.log(datax);
+
+  useEffect(() => {
+    getCategories().then((res) => setData(res));
+  }, []);
   let history = useHistory();
   const { t, i18n } = useTranslation();
   return (
     <div
-    id="/#cat"
+      id="/#cat"
       className=" main-category-container"
       style={({ backgroundColor: "#e8e8e8" }, style)}
     >
@@ -99,18 +108,19 @@ function CategoryList({ data = [], style, title }) {
         className="row"
         style={{ display: "flex", justifyContent: "center" }}
       >
-        {datax.map((ele, index) => (
-          <div
-            onClick={() => {
-              history.push("/list?q=None&c=categoryName");
-            }}
-            style={{ width: "120px" }}
-            key={index}
-            className="border-item"
-          >
-            <Image src={ele.image} />
-          </div>
-        ))}
+        {datax &&
+          datax.map((ele, index) => (
+            <div
+              onClick={() => {
+                history.push("/list?q=None&c=categoryName");
+              }}
+              style={{ width: "120px" }}
+              key={index}
+              className="border-item"
+            >
+              <Image src={ele.image} />
+            </div>
+          ))}
       </div>
     </div>
   );
